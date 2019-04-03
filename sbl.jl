283958d2966e704c5a3e7a9fa3193b0211a824c5
Matt O'Shaughnessy <matthewoshaughnessy@gatech.edu>
2019-04-03 19:43:43 -0400

    Fixed discrepancy in weight calculation for SBL-RWL1

--- sbl.jl
+++ sbl.jl
@@ -6,23 +6,24 @@ using Convex
 
 
 """
-SBL_EM : implements SBL using the EM updates of [1]
+Implements SBL using the EM updates of [1]
 Inputs:
-  y        : measurements, m x 1
-  Φ        : dictionary, m x n
-  σ2init   : initial value of noise variance parameter (default: 1e-3)
-  updateσ2 : update σ2 during EM procedure? (default: true)
-  τ        : threshold on γ used for pruning (default: 1e4)
-  tol      : convergence tolerance (default: 1e-8)
-  maxiter  : maximum iteration count (default: 100)
-  verbose  : print debug information at each iteration (default: false)
+  `y`        : measurements, m x 1
+  `Φ`        : dictionary, m x n
+  `σ2init`   : initial value of noise variance parameter (default: 1e-3)
+  `updateσ2` : update σ2 during EM procedure? (default: true)
+  `τ`        : threshold on γ used for pruning (default: 1e4)
+  `tol`      : convergence tolerance (default: 1e-8)
+  `maxiter`  : maximum iteration count (default: 100)
+  `verbose`  : print debug information at each iteration (default: false)
+  `debug`    : calculate and output cost function value at each iteration (default: false)
 Outputs:
-  xhat     : SBL estimate of x (i.e., posterior mean at convergence)
+  `xhat`     : SBL estimate of x (i.e., posterior mean at convergence)
 Reference:
-  [1]      : M Tipping, 'Sparse Bayesian learning and the relevance vector machine,'
-             J Machine Learning Research, 2001.
+  [1]        : M Tipping, 'Sparse Bayesian learning and the relevance vector machine,'
+               J Machine Learning Research, 2001.
 """
-function sbl_em(y, Φ; σ2init = 1e-3, updateσ2 = true, τ = 1e4, tol = 1e-8, maxiter = 100, verbose = false)
+function sbl_em(y, Φ; σ2init = 1e-3, updateσ2 = true, τ = 1e8, tol = 1e-8, maxiter = 100, verbose = false, debug = false)
 
     # initialization
     m, n = size(Φ)
@@ -33,6 +34,7 @@ function sbl_em(y, Φ; σ2init = 1e-3, updateσ2 = true, τ = 1e4, tol = 1e-8, m
     iter = 1
     converged = false
     mackay = zeros(n)
+    cost = debug ? zeros(maxiter) : nothing
 
     # parameters
     a = zeros(n)
@@ -69,7 +71,11 @@ function sbl_em(y, Φ; σ2init = 1e-3, updateσ2 = true, τ = 1e4, tol = 1e-8, m
         converged = norm(γ-γ_old) < tol || iter >= maxiter
         if verbose
             @printf("%d coefficients in model; ", Int(sum(γsupp)));
-            @printf("||γ-γold|| = %.3e.\n", norm(γ-γ_old));
+            @printf("||γ|| = %.3e, ||γ-γold|| = %.3e.\n", norm(γ), norm(γ-γ_old));
+        end
+        if debug
+          C = λ*I + Φ*Diagonal(γ.^-1)*Φ'
+          cost[iter] = log(det(C)) + y'*inv(C)*y
         end
         iter += 1
 
@@ -80,51 +86,54 @@ function sbl_em(y, Φ; σ2init = 1e-3, updateσ2 = true, τ = 1e4, tol = 1e-8, m
     μ[γsupp] = 1/σ2*Σ*Φ[:,γsupp]'*y
     μ[.!γsupp] = zeros(sum(.!γsupp))
     xhat = μ
+    cost = debug ? cost[1:iter-1] : nothing
 
-    return xhat, σ2
+    return xhat, σ2, cost
 
 end
 
 
 """
-SBL_RWL1 : implements SBL using the RWL1 updates of [2]
+Implements SBL using the RWL1 updates of [2]
 Inputs:
-  y        : measurements, m x 1
-  Φ        : dictionary, m x n
-  λ        : data fidelity/sparsity tuning parameter
-  τ        : threshold on γ used for pruning (default: 1e4)
-  tol      : convergence tolerance (default: 1e-8)
-  maxiter  : maximum iteration count (default: 100)
-  verbose  : print debug information at each iteration (default: false)
+  `y`        : measurements, m x 1
+  `Φ`        : dictionary, m x n
+  `λ`        : data fidelity/sparsity tuning parameter
+  `τ`        : threshold on γ used for pruning (default: 1e4)
+  `tol`      : convergence tolerance (default: 1e-8)
+  `maxiter`  : maximum iteration count (default: 100)
+  `verbose`  : print debug information at each iteration (default: false)
+  `debug`    : calculate and output cost function value at each iteration (default: false)
 Outputs:
-  xhat     : SBL-RWL1 estimate of x
-  Xhat     : history of iterate for x, n x niter
-  What     : history of iterate for w, n x niter
+  `xhat`     : SBL-RWL1 estimate of x
+  `Xhat`     : history of iterate for x, n x niter
+  `What`     : history of iterate for w, n x niter
 Reference:
-  [2]      : D Wipf and S Nagarajan, 'Iterative reweighted l1 and l2 methods
-             for finding sparse solutions,' IEEE J Selected Topics in Signal
-             Processing, 2010.
+  [2]        : D Wipf and S Nagarajan, 'Iterative reweighted l1 and l2 methods
+               for finding sparse solutions,' IEEE J Selected Topics in Signal
+               Processing, 2010.
 """
-function sbl_rwl1(y, Φ, λ; τ = 1e-8, tol = 1e-8, maxiter = 100, verbose = false)
+function sbl_rwl1(y, Φ, λ; τ = 1e-8, tol = 1e-8, maxiter = 100, verbose = false, debug = false)
 
     # initialization
     m, n = size(Φ)
     x = zeros(n)
     γ = ones(n)
-    z = ones(n)
+    z = 1/4*ones(n) # TODO (makes weights in first iteration 0.5)
     γold = zeros(n)
     γsupp = trues(n)
     Xhat = zeros(n,maxiter)
     Γhat = zeros(n,maxiter)
     iter = 1
     converged = false
+    cost = debug ? zeros(maxiter) : nothing
 
     # perform RWL1 iterations
     while !converged
 
         verbose ? print("Iteration $(iter): ") : nothing;
 
-        # update γ
+        # update x
         w = sqrt.(z[γsupp])
         xc = Variable(Int(sum(γsupp)))
         problem = minimize( sumsquares(y-Φ[:,γsupp]*xc) + 2*λ*sum(w.*abs(xc)) )
@@ -132,6 +141,8 @@ function sbl_rwl1(y, Φ, λ; τ = 1e-8, tol = 1e-8, maxiter = 100, verbose = fal
         xc = evaluate(xc)
         x = zeros(n)
         x[γsupp] = xc
+
+        # update γ
         γ[γsupp] = z[γsupp].^(-1/2) .* abs.(x[γsupp])
         γsupp = abs.(γ) .>= τ
 
@@ -145,6 +156,10 @@ function sbl_rwl1(y, Φ, λ; τ = 1e-8, tol = 1e-8, maxiter = 100, verbose = fal
         if verbose
             @printf("%d coefficients in model; ", Int(sum(γsupp)));
             @printf("||γ-γold|| = %.3e.\n", norm(γ-γold));
+        end
+        if debug
+          C = λ*I + Φ*Diagonal(γ)*Φ'
+          cost[iter] = log(det(C)) + y'*inv(C)*y
         end
         iter += 1
         γold = γ[:]
@@ -160,6 +175,93 @@ function sbl_rwl1(y, Φ, λ; τ = 1e-8, tol = 1e-8, maxiter = 100, verbose = fal
     xhat = x
     Xhat = Xhat[:,1:iter-1]
     Γhat = Γhat[:,1:iter-1]
-    return xhat, Xhat, Γhat
+    cost = debug ? cost[1:iter-1] : nothing
+
+    return xhat, Xhat, Γhat, cost
+
+end
+
+"""
+Implements SBL using the RWL1 updates of [2]. Directly calculates weights without using γ (except for debug).
+Inputs:
+  `y`        : measurements, m x 1
+  `Φ`        : dictionary, m x n
+  `λ`        : data fidelity/sparsity tuning parameter
+  `τ`        : threshold on γ used for pruning (default: 1e4)
+  `tol`      : convergence tolerance (default: 1e-8)
+  `maxiter`  : maximum iteration count (default: 100)
+  `verbose`  : print debug information at each iteration (default: false)
+  `debug`    : calculate and output cost function value at each iteration (default: false)
+Outputs:
+  `xhat`     : SBL-RWL1 estimate of x
+  `Xhat`     : history of iterate for x, n x niter
+  `What`     : history of iterate for w, n x niter
+Reference:
+  [2]        : D Wipf and S Nagarajan, 'Iterative reweighted l1 and l2 methods
+               for finding sparse solutions,' IEEE J Selected Topics in Signal
+               Processing, 2010.
+"""
+function sbl_rwl1_simple(y, Φ, λ; τ = 1e-8, tol = 1e-8, maxiter = 100, verbose = false, debug = false)
+
+  # initialization
+  m, n = size(Φ)
+  x = zeros(n)
+  w = ones(n)
+  γ = ones(n)
+  γold = zeros(n)
+  γsupp = trues(n)
+  Xhat = zeros(n,maxiter)
+  What = zeros(n,maxiter)
+  iter = 1
+  converged = false
+  cost = debug ? zeros(maxiter) : nothing
+
+  # perform RWL1 iterations
+  while !converged
+
+      verbose ? print("Iteration $(iter): ") : nothing;
+
+      # update x
+      xc = Variable(Int(sum(γsupp)))
+      problem = minimize( sumsquares(y-Φ[:,γsupp]*xc) + λ*sum(w[γsupp].*abs(xc)) )
+      solve!(problem, SCSSolver(verbose = false), verbose = false)
+      xc = evaluate(xc)
+      x = zeros(n)
+      x[γsupp] = xc
+
+      # update w
+      w[γsupp] = 2*sqrt.(diag(Φ[:,γsupp]'*inv(λ*I+Φ[:,γsupp]*Diagonal(γ[γsupp])*Φ[:,γsupp]')*Φ[:,γsupp]))
+      γ = 2*abs.(x) ./ w
+      γsupp = γ .>= τ
+
+      # check convergence
+      Xhat[:,iter] = x
+      What[γsupp,iter] = w[γsupp]
+      converged = (iter > 1 && norm(γ-γold) < tol) || iter >= maxiter
+      if verbose
+          @printf("%d coefficients in model; ", Int(sum(γsupp)));
+          @printf("||γ-γold|| = %.3e.\n", norm(γ-γold));
+      end
+      if debug
+        C = λ*I + Φ*Diagonal(γ)*Φ'
+        cost[iter] = log(det(C)) + y'*inv(C)*y
+      end
+      iter += 1
+      γold = γ[:]
+
+  end
+
+  if verbose && iter <= maxiter
+      println("Converged in $(iter-1) iterations.")
+  elseif verbose
+      println("Reached maximum iterations.")
+  end
+
+  xhat = x
+  Xhat = Xhat[:,1:iter-1]
+  What = hcat(ones(n), What[:,1:iter-1])
+  cost = cost[1:iter-1]
+
+  return xhat, Xhat, What, cost
 
 end
--- test_sbl_rwl1.jl
+++ test_sbl_rwl1.jl
@@ -9,7 +9,7 @@ using Plots, PlotThemes
 # --- generate measurements ---
 m = 32;
 n = 64;
-s = 12;
+s = 13;
 Random.seed!(0);
 x = zeros(n);
 x[rand(1:n,s)] = ones(s);
@@ -20,7 +20,7 @@ y = Φ*x;
 b = Φ'*y;
 G = Φ'*Φ;
 λ = 0.01;
-niter = Dict("EM" => 100, "RWL1" => 100);
+maxiter = Dict("EM" => 1000, "RWL1" => 100);
 
 
 
@@ -35,16 +35,25 @@ err_bpdn = rerr(xhat_bpdn)
 
 # --- solve with SBL using EM iterations ---
 include("sbl.jl");
-xhat_sbl, σ2_sbl = sbl_em(y, Φ, σ2init = 2*λ, updateσ2 = false,
-    maxiter = niter["EM"], verbose = false);
+xhat_sbl, σ2_sbl, cost_sbl = sbl_em(y, Φ, σ2init = λ, updateσ2 = false,
+    maxiter = maxiter["EM"], verbose = true, debug = true);
 err_sbl = rerr(xhat_sbl)
 
 
 
 # --- solve with SBL using RWL1 iterations ---
 include("sbl.jl");
-xhat_sblrwl1, Xhat_sblrwl1, Γhat_sblrwl1 = sbl_rwl1(y, Φ, λ, maxiter = 100, verbose = true);
+xhat_sblrwl1, Xhat_sblrwl1, Γhat_sblrwl1, cost_sblrwl1 = sbl_rwl1(y, Φ, λ, τ = 0,
+    maxiter = maxiter["RWL1"], verbose = true, debug = true);
 errs_sblrwl1 = [rerr(Xhat_sblrwl1[:,i]) for i in 1:size(Xhat_sblrwl1,2)]
+
+
+
+# --- solve with SBL using RWL1 iterations (no latent variables) ---
+include("sbl.jl");
+xhat_sblrwl1_simple, Xhat_sblrwl1_simple, What_sblrwl1_simple, cost_sblrwl1_simple = sbl_rwl1_simple(y, Φ, λ, τ = 0,
+    maxiter = maxiter["RWL1"], verbose = true, debug = true);
+errs_sblrwl1_simple = [rerr(Xhat_sblrwl1_simple[:,i]) for i in 1:size(Xhat_sblrwl1_simple,2)]
 
 
 
@@ -54,7 +63,8 @@ for i in 1:n
     if x[i] != 0
         plot!(p,log10.([1; Γhat_sblrwl1[i,:]]), color=RGBA(0.3,0.1,1.0,0.8))
     else
-        plot!(p,log10.([1; Γhat_sblrwl1[i,:]]), xlab = "RWL1 iteration", ylab = "log \\gamma_i", color=RGBA(1.0,0.1,0.3,0.8))
+        plot!(p,log10.([1; Γhat_sblrwl1[i,:]]), xlab = "RWL1 iteration", ylab = "log \\gamma_i", 
+            color=RGBA(1.0,0.1,0.3,0.8))
     end
 end
 display(p)
