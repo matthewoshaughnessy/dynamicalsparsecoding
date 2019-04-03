fa7cf64d4f6a22f6e5df9354f26f4eb980207c52
Matt O'Shaughnessy <matthewoshaughnessy@gatech.edu>
2019-04-03 18:07:57 -0400

    Clean up iterative and dynamic RWL1-LCA implementations

--- lca.jl
+++ lca.jl
@@ -2,17 +2,19 @@
 
 using DynamicalSystems
 
+"""
+Implements a standard LCA with generic thresholding function
+Inputs:
+  `b`  : Φ^T*y
+  `G`  : Φ^T*Φ
+  `T`  : thresholding function, x = T(u)
+  `τ`  : LCA time constant (used in ODE definition of LCA, not ODE solver)
+  `tf` : amount of time to simulate
+  `u0` : initial state for u
+Outputs:
+  `x`  : trajectory of x, n x nt
+"""
 function lca(b, G, T, τ, tf, u0)
-# lca : implements a standard LCA with generic thresholding function
-# Inputs:
-#   b  : Φ^T*y
-#   G  : Φ^T*Φ
-#   T  : thresholding function, x = T(u)
-#   τ  : LCA time constant (used in ODE definition of LCA, not ODE solver)
-#   tf : amount of time to simulate
-#   u0 : initial state for u
-# Outputs:
-#   x  : trajectory of x, n x nt
 
     eom(u,p,t) = SVector{n}(1/τ*(b-u-(G-I)*T(u)));
     lca = ContinuousDynamicalSystem(eom, u0, nothing);
@@ -23,8 +25,9 @@ function lca(b, G, T, τ, tf, u0)
 
 end
 
-function wlca(b, G, T, τ, tf, u0, w)
-# wlca : implements a weighted LCA with fixed weights and generic thresholding function
+function lca_j(b, G, T, τ, tf, u0)
+# lca : implements a standard LCA with generic thresholding function
+#       with Jacobian specified
 # Inputs:
 #   b  : Φ^T*y
 #   G  : Φ^T*Φ
@@ -32,9 +35,43 @@ function wlca(b, G, T, τ, tf, u0, w)
 #   τ  : LCA time constant (used in ODE definition of LCA, not ODE solver)
 #   tf : amount of time to simulate
 #   u0 : initial state for u
-#   w  : fixed weights, n x 1
 # Outputs:
 #   x  : trajectory of x, n x nt
+# TODO
+    
+    #function eom_lca_j!(du, u, p, t)
+    #    du = 1/τ*(b-u-(G-I)*T(u))
+    #    return
+    #end
+    eom(u,p,t) = SVector{n}(1/τ*(b-u-(G-I)*T(u)));
+
+    #function eom_jac(J, x, p, n)
+    #    J = Diagonal(ones(2))
+    #    return
+    #end
+
+    lca = ContinuousDynamicalSystem(eom, u0, nothing);
+    u = Matrix(trajectory(lca, tf))';
+    x = T(u);
+    
+    return x, u
+
+end
+
+"""
+wlca : implements a weighted LCA with fixed weights and generic thresholding function
+Inputs:
+  `b`  : Φ^T*y
+  `G`  : Φ^T*Φ
+  `T`  : thresholding function, x = T(u)
+  `τ`  : LCA time constant (used in ODE definition of LCA, not ODE solver)
+  `tf` : amount of time to simulate
+  `u0` : initial state for u
+  `w`  : fixed weights, n x 1
+Outputs:
+  `x`  : trajectory of x, n x nt
+"""
+function wlca(b, G, T, τ, tf, u0, w)
 
     eom(u,p,t) = SVector{n}(1/τ*(b-u-(G-I)*T(u,p)));
     lca = ContinuousDynamicalSystem(eom, u0, w);
@@ -45,21 +82,23 @@ function wlca(b, G, T, τ, tf, u0, w)
 
 end
 
+"""
+Implements a dynamic rwl1 LCA, with generic thresholding function
+Inputs:
+  `b`  : Φ^T*y
+  `G`  : Φ^T*Φ
+  `T`  : thresholding function, x = T(u)
+  `τu` : LCA time constant for state u (used in ODE definition of LCA, not ODE solver)
+  `τw` : LCA time constant for weights w (used in ODE definition of LCA, not ODE solver)
+  `tf` : amount of time to simulate
+  `u0` : initial state for u
+  `w0` : initial state for w
+  `ε`  : constant used in reweighting (default: 0.01)
+Outputs:
+  `x`  : trajectory of x, n x nt
+  `w`  : trajectory of w, n x nt
+"""
 function drwl1lca(b, G, T, τu, τw, tf, u0, w0, ε = 0.01)
-# drwl1lca : implements a dynamic rwl1 LCA, with generic thresholding function
-# Inputs:
-#   b  : Φ^T*y
-#   G  : Φ^T*Φ
-#   T  : thresholding function, x = T(u)
-#   τu : LCA time constant for state u (used in ODE definition of LCA, not ODE solver)
-#   τw : LCA time constant for weights w (used in ODE definition of LCA, not ODE solver)
-#   tf : amount of time to simulate
-#   u0 : initial state for u
-#   w0 : initial state for w
-#   ε  : constant used in reweighting (default: 0.01)
-# Outputs:
-#   x  : trajectory of x, n x nt
-#   w  : trajectory of w, n x nt
 
     n = size(G,1);
     function eom(uu,p,t) # uu : combined state [u; w]
@@ -75,6 +114,96 @@ function drwl1lca(b, G, T, τu, τw, tf, u0, w0, ε = 0.01)
     u = uu[1:n,:];
     w = uu[n+1:end,:];
     x = T(u,w);
+    return x, w
+
+end
+
+"""
+Implements a dynamic rwl1 LCA, with SBL-RWL1 thresholding function, using first-differences
+Inputs:
+  `b`  : Φ^T*y
+  `Φ`  : dictionary
+  `λ`  : SBL regularization parameter
+  `τu` : LCA time constant for state u (used in ODE definition of LCA, not ODE solver)
+  `τw` : LCA time constant for weights w (used in ODE definition of LCA, not ODE solver)
+  `nt` : number of time steps to simulate
+  `u0` : initial state for u
+  `w0` : initial state for w
+Outputs:
+  `x`  : trajectory of x, n x nt
+  `w`  : trajectory of w, n x nt
+"""
+function dsblrwl1lcafd(b, Φ, λ, τu, τw, nt, u0, w0)
+
+    n = size(Φ,2)
+    G = Φ'*Φ
+    Tw(u,w) = (u .- λ*w.*sign.(u)) .* (u .>= λ*w)
+    function dudw(u,w)
+        du = 1/τu*(b-u-(G-I)*Tw(u,w))
+        dw = 1/τw*(w .- (2*sqrt.(diag(Φ'*inv(λ*I+Φ*Diagonal(abs.(Tw(u,w))./w)*Φ')*Φ))))
+        return du, dw
+    end
+
+    u = zeros(n,nt)
+    w = zeros(n,nt)
+    duhist = zeros(n,nt) # DEBUG
+    dwhist = zeros(n,nt) # DEBUG
+    u[:,1] = u0
+    w[:,1] = w0
+
+    for i in 2:nt
+        du, dw = dudw(u[:,i],w[:,i])
+        duhist[:,i-1] = du # DEBUG
+        dwhist[:,i-1] = dw # DEBUG
+        u[:,i] = u[:,i-1] + du
+        w[:,i] = w[:,i-1] + dw
+    end
+
+    x = zeros(n,nt)
+    for i in 1:nt
+        x[:,i] = Tw(u[:,i],w[:,i])
+    end
+    xhat = x[:,end]
+    return xhat, x, w, u, dwhist, duhist
+
+end
+
+"""
+Implements a dynamic rwl1 LCA, with SBL-RWL1 thresholding function
+Inputs:
+  `b`  : Φ^T*y
+  `Φ`  : dictionary
+  `λ`  : SBL regularization parameter
+  `τu` : LCA time constant for state u (used in ODE definition of LCA, not ODE solver)
+  `τw` : LCA time constant for weights w (used in ODE definition of LCA, not ODE solver)
+  `tf` : amount of time to simulate
+  `u0` : initial state for u
+  `w0` : initial state for w
+Outputs:
+  `x`  : trajectory of x, n x nt
+  `w`  : trajectory of w, n x nt
+"""
+function dsblrwl1lca(b, Φ, λ, τu, τw, tf, u0, w0)
+
+    n = size(Φ,2)
+    G = Φ'*Φ
+    Tw(u,w) = (u .- λ*w.*sign.(u)) .* (u .>= λ*w)
+    function eom(duu, uu, p, t) # uu : combined state [u; w]
+        u = uu[1:n]
+        w = uu[n+1:end]
+        du = 1/τu*(b-u-(G-I)*Tw(u,w))
+        dw = 1/τw*(w.^-1 .- (2*sqrt.(diag(Φ'*inv(λ*I+Φ*Diagonal(abs.(Tw(u,w))./w)*Φ')*Φ))).^-1)
+        duu[1:n] = du
+        duu[n+1:end] = dw
+        return
+    end
+    uu0 = [u0; w0]
+    lca = ContinuousDynamicalSystem(eom, uu0, nothing)
+    #lca = DiscreteDynamicalSystem(eom, uu0, nothing)
+    uu = Matrix(trajectory(lca,tf))'
+    u = uu[1:n,:]
+    w = uu[n+1:end,:]
+    x = Tw(u,w)
     return x, w
 
 end
--- test_lca_rwl1.jl
+++ test_lca_rwl1.jl
@@ -1,9 +1,11 @@
-# Compares three implementations of RWL1:
-#   1. standard iterative reweighted l1 (using Convex.jl)
-#   2. RWL1 implemented using a sequence of weighted-BPDN LCAs
-#   3. RWL1 implemented using a single dynamic reweighted-BPDN LCA
-# Generates:
-#   - figs/lca_rwl1_dyn_vs_iter.pdf
+"""
+Compares three implementations of separable RWL1:
+  1. standard iterative reweighted l1 (using Convex.jl)
+  2. RWL1 implemented using a sequence of weighted-BPDN LCAs
+  3. RWL1 implemented using a single dynamic reweighted-BPDN LCA
+Generates:
+  - figs/lca_rwl1_dyn_vs_iter.pdf
+"""
 
 
 
@@ -37,22 +39,22 @@ nrw = 3;
 
 
 
-# --- BPDN ---
-W_bpdn = zeros(n,nrw+1);
-W_bpdn[:,1] = ones(n);
-Xhat_bpdn = zeros(n,nrw);
+# --- iterative RWL1 (separable) ---
+W_rwl1 = zeros(n,nrw+1);
+W_rwl1[:,1] = ones(n);
+Xhat_rwl1 = zeros(n,nrw);
 for i ∈ 1:nrw
     xhat = Variable(n);
-    problem = minimize( 1/2*sumsquares(y-Φ*xhat) + λ0*sum(W_bpdn[:,i].*abs(xhat)) );
+    problem = minimize( 1/2*sumsquares(y-Φ*xhat) + λ0*sum(W_rwl1[:,i].*abs(xhat)) );
     solve!(problem,SCSSolver(verbose=false),verbose=false);
-    Xhat_bpdn[:,i] = evaluate(xhat);
-    W_bpdn[:,i+1] = 1 ./ (abs.(Xhat_bpdn[:,i]) .+ ε);
+    Xhat_rwl1[:,i] = evaluate(xhat);
+    W_rwl1[:,i+1] = 1 ./ (abs.(Xhat_rwl1[:,i]) .+ ε);
 end
-errs_bpdn = [rerr(Xhat_bpdn[:,i]) for i ∈ 1:nrw];
+errs_rwl1 = [rerr(Xhat_rwl1[:,i]) for i ∈ 1:nrw];
 
 
 
-# --- Iterative LCA ---
+# --- Iterative LCA (separable) ---
 include("lca.jl");
 Tw(u,w) = (u .- λ0*w.*sign.(u)) .* (u .>= λ0*w);
 W_ilca = zeros(n,nrw+1);
@@ -68,9 +70,9 @@ errs_ilca = [rerr(Xhat_ilca[:,i]) for i ∈ 1:size(Xhat_ilca,2)];
 
 
 
-# --- dynamic LCA ---
+# --- dynamic LCA (separable) ---
 include("lca.jl");
-Xhat_dlca, W_dlca = dlca(b, G, Tw, τ, τ, (nτ-1)*nrw*τ, zeros(n), λ0*ones(n), ε);
+Xhat_dlca, W_dlca = drwl1lca(b, G, Tw, τ, τ, (nτ-1)*nrw*τ, zeros(n), λ0*ones(n), ε);
 errs_dlca = [rerr(Xhat_dlca[:,i]) for i in 1:size(Xhat_dlca,2)];
 
 
@@ -83,7 +85,7 @@ p = plot(log.(errs_dlca), label = "LCA (dynamic reweighting)", lw = 2,
 plot!(p, log.(errs_ilca), label = "LCA (iterative reweighting)", lw = 2,
     line = :dash);
 for i in 1:nrw
-    plot!(p, [(i-1)*nτ; i*nτ], log(errs_bpdn[i])*ones(2),
+    plot!(p, [(i-1)*nτ; i*nτ], log(errs_rwl1[i])*ones(2),
         label = "Iterative RWL1: reweight $(i)", line = :dot, lw = 2);
 end
 p
